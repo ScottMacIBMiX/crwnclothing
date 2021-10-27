f797dc40a208d5e4ee2dcfc425bb7606dcfb7d77
Scott MacDonald <scottmacdonald@192.168.1.253>
2021-10-27 15:45:41 +0100

    Header and navigation bar added

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { Route, Switch } from "react-router-dom";
 
 import Homepage from "./pages/Homepage/Homepage";
 import ShopPage from "./pages/Shop/Shop";
+import Header from "./components/Header/Header";
 
 
 function App() {
   return (
     <div className="App">
+    <Header/>
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route exact path="/shop" component={ShopPage} />
